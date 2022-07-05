dc4e53a503e665446d080188c2821e774323dfe9
Amili-usain <amilihussein073@gmail.com>
2022-07-05 03:06:59 -0700

    A program that prints _putchar, followed by a new line.

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include<amili.h>
+/**
+ * main - Prints _putchar, followed by a new line.
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char str[] = "_putchar\n";
+	int i = 0;
+
+	while (str[] != '\n')
+	{
+		char c = str[i];
+
+		_putchar(c);
+		i++;
+	}
+	return (0);
+}
