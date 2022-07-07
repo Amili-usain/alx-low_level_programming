89493ac07bc16886b4325bb1797027276910973d
Amili-usain <amilihussein073@gmail.com>
2022-07-07 04:17:08 -0700

    adding 5-more_numbers.c file

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * more_numbers - prints 10 times numbers between 0 to 14
+ */
+void more_numbers(void)
+{
+	int n, count;
+
+	for (count = 48; count <= 57; count++)
+	{
+		for (n = 48; n <= 62; n++)
+		{
+			if (n > 57)
+				_putchar((n / 10) + 48);
+			_putchar((n % 10) + 48);
+		}
+		_putchar('\n');
+	}
+}
