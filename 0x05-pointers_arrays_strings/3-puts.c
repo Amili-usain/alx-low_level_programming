0138be4526a8898cfb571c8ced94abb095a07509
Amili-usain <amilihussein073@gmail.com>
2022-07-11 03:16:25 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string, followed by a new line, to stdout
+ * @str: The string to be printed.
+ */
+void _puts(char *str)
+{
+	while (*str)
+		_putchar(*str++);
+
+	_putchar('\n');
+}
