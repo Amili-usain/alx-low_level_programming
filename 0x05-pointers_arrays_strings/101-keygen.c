620d007c47c0ead74c204102ec437c382c1ab0ca
Amili-usain <amilihussein073@gmail.com>
2022-07-11 13:15:02 -0700

    advanced task 2

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - generates random valid passwords for the program 101-crackme.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int r = 0, s = 0;
+	time_t t;
+
+	srand((unsigned int) time(&t));
+	while (s < 2772)
+	{
+		r = rand() % 128;
+		if ((s + r) > 2772)
+			break;
+		s = s + r;
+		printf("%d", r);
+	}
+	printf("%d\n", (2772 - s));
+	return (0);
+}
