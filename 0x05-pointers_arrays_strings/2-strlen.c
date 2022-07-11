932698d6296476f24dbb783e4dc90e439fa7b86e
Amili-usain <amilihussein073@gmail.com>
2022-07-11 03:09:30 -0700

    2-strlen.c file

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - Returns the length of a string.
+ * @s: input string.
+ * Return: length of a string.
+ *
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s++)
+		length++;
+
+	return (length);
+}
