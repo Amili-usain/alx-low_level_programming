3a2caf36d263932c306e14730d4bd665ea966299
Amili-usain <amilihussein073@gmail.com>
2022-07-18 15:44:49 +0300

    Task 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,36 @@
+/*
+ * File: 3+strspn.c
+ * Auth: Amili H
+ * Task: writing a function that gets the length of a prefix substring.
+ */
+#include "main.h"
+
+/**
+ * _strspn - Gets the length of a prefix substring
+ * @s: The string to be searched.
+ * @accept: The prefix to be measured.
+ *
+ * Return: The number of bytes in s which
+ * consist only of bytes from accept.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int b = 0;
+	int i;
+
+	while (*s)
+	{
+		for (i = 0; accept[i]; i++)
+		{
+			if (*s == accept[i])
+			{
+				b++;
+				break;
+			}
+			else if (accept[i + 1] == '\0')
+				return (b);
+		}
+		s++;
+	}
+	return (b);
+}
