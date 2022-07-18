cbb3218f4c99a04502851f35448b359bd8cdc8ce
Amili-usain <amilihussein073@gmail.com>
2022-07-18 14:52:03 +0300

    Task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,30 @@
+/*
+ * File: 2-strchr.c
+ * Auth: Amili H
+ * Task: writing a function that locates a character in a string.
+ */
+#include "main.h"
+
+/**
+ * _strchr - Locates a character in a string.
+ * @s: The string to be searched.
+ * @c: The character to be located.
+ *
+ * Return: *s
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	if (*s == c)
+	{
+		return (s);
+	}
+	return (0);
+}
