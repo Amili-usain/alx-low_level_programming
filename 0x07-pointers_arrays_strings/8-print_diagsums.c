b2dbd45e880ee92c2b955ba87e811679ea93a1c8
Amili-usain <amilihussein073@gmail.com>
2022-07-18 14:36:59 -0700

    Task 8

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,29 @@
+/*
+ * File: 8-print_diagsums.c
+ * Auth: Amili H
+ * Task: writing a function that prints the sum of two diagonals
+ *        of a square matrix of integers.
+ */
+
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - Prints the sum of the two diagonals
+ *                  of a square matrix of integers.
+ * @a: The matrix of integers.
+ * @size: The size of the matrix.
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i, sum1 = 0, sum2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		sum1 += *(a + (size * i + i));
+		sum2 += *(a + (size * i + size - 1 - i));
+	}
+	printf("%d, ", sum1);
+	printf("%d\n", sum2);
+}
