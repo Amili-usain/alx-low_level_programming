b77c2b201fd292bd5f80dbc8ac582197f8959852
Amili-usain <amilihussein073@gmail.com>
2022-07-18 17:03:37 +0300

    Update 4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -4,7 +4,7 @@
  * Task: writing a function that searches a string for any of a set of bytes.
  */
 
-/** 
+/**
  * _strpbrk - Searches a string for any of a set of bytes.
  * @s: The string to be searched.
  * @accept: The set of bytes to be searched for.
