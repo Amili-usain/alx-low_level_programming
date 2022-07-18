cc2d031f88bd9df167b54891a7e252bb8ad18afa
Amili-usain <amilihussein073@gmail.com>
2022-07-18 22:55:53 +0300

    Create 7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,24 @@
+/*
+ * File: 7-print_chessboard.c
+ * Auth: Amili H
+ * Task: writing a function that prints the chessboard.
+ */
+
+#include "main.h"
+
+/**
+ * print_chessboard - Prints a chessboard.
+ * @a: The chessboard to be printed.
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+			_putchar(a[i][j]);
+
+		_putchar('\n');
+	}
+}
