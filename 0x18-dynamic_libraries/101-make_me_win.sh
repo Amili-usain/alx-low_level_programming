0290a9fc8f372e4aa8e80576e8656a80251de8dd
Amili-usain <amilihussein073@gmail.com>
2022-10-03 21:26:39 +0300

    Delete 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-wget -P .. https://raw.githubusercontent.com/monoprosito/holbertonschool-low_
-level_programming/master/0x18-dynamic_libraries/libgiga.so
-export LD_PRELOAD="$PWD/../libgiga.so
