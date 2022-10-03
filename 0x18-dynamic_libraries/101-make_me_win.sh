73a24287f0f7f3b0b25ccefd9e701337e17a282a
Amili-usain <amilihussein073@gmail.com>
2022-10-03 11:24:15 -0700

    advanced task 2

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-wget -P .. https://raw.githubusercontent.com/monoprosito/holbertonschool-low_level_programming/master/0x18-dynamic_libraries/libgiga.so
+wget -P .. https://raw.githubusercontent.com/monoprosito/holbertonschool-low_
+level_programming/master/0x18-dynamic_libraries/libgiga.so
 export LD_PRELOAD="$PWD/../libgiga.so
