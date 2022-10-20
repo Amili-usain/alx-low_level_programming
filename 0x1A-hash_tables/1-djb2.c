f2c446ccfeb6a3a0297e22702ecf85001358ff64
Amili-usain <amilihussein073@gmail.com>
2022-10-20 13:41:30 -0700

    Mandatory tasks 0-6

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,12 +1,17 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - hash function generates a djb2
+ * @str: pointer to chars a hash
+ * Return: hash key
+ */
 unsigned long int hash_djb2(const unsigned char *str)
 {
-    unsigned long int hash;
-    int c;
+	unsigned long hash = 5381;
+	int c;
 
-    hash = 5381;
-    while ((c = *str++))
-    {
-        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-    }
-    return (hash);
+	while ((c = *str++))
+	hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+
+	return (hash);
 }
